a46fb7142b091bb1c7c1b48d9bc5e2a01466a772
Patrick Beard <pcbeard@mac.com>
2016-03-26 05:25:48 -0700

    Track history by player, save in history.yml
    Inverted how chat message are handled. All messages are treated as
    commands, unless they start with a single quote character. Much more
    useful.

--- src/main/java/ru/nukkit/space2slash/Space2Slash.java
+++ src/main/java/ru/nukkit/space2slash/Space2Slash.java
@@ -1,5 +1,6 @@
 package ru.nukkit.space2slash;
 
+import cn.nukkit.Player;
 import cn.nukkit.command.Command;
 import cn.nukkit.command.CommandSender;
 import cn.nukkit.event.EventHandler;
@@ -8,18 +9,53 @@ import cn.nukkit.event.Listener;
 import cn.nukkit.event.player.PlayerChatEvent;
 import cn.nukkit.event.player.PlayerCommandPreprocessEvent;
 import cn.nukkit.plugin.PluginBase;
+import cn.nukkit.utils.Config;
 import cn.nukkit.utils.TextFormat;
 
+import java.io.File;
+
 import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.ListIterator;
 
 public class Space2Slash extends PluginBase implements Listener {
-    private ArrayList<String> history = new ArrayList<String>();
+    private HashMap<String, ArrayList<String>> historyByPlayer = new HashMap<String, ArrayList<String>>();
+    private Config historyConfig;
+
+    private ArrayList<String> historyForPlayer(Player player) {
+      ArrayList<String> history = historyByPlayer.get(player.getName());
+      if (history == null) {
+        history = new ArrayList<String>();
+        historyByPlayer.put(player.getName(), history);
+      }
+      return history;
+    }
+    
+    private void saveHistory() {
+      for (String key : historyByPlayer.keySet()) {
+        historyConfig.set(key, historyByPlayer.get(key));
+      }
+      historyConfig.save();
+    }
 
     @Override
     public void onEnable() {
         this.getServer().getPluginManager().registerEvents(this, this);
+
+        // ensure we have a writable directory in plugins
+        this.getLogger().info(String.valueOf(this.getDataFolder().mkdirs()));
+
+        historyConfig = new Config(new File(this.getDataFolder(), "history.yml"), Config.YAML);
+        for (String key : historyConfig.getAll().keySet()) {
+          historyByPlayer.put(key, (ArrayList<String>)historyConfig.getStringList(key));
+        }
+    }
+    
+    @Override
+    public void onDisable() {
+      saveHistory();
     }
 
     static Iterable<String> reversed(final ArrayList<String> original) {
@@ -35,7 +71,8 @@ public class Space2Slash extends PluginBase implements Listener {
       };
     }
     
-    String preprocessCommand(String message) {
+    String preprocessCommand(Player player, String message) {
+      ArrayList<String> history = historyForPlayer(player);
       if (message.startsWith("!") && !history.isEmpty()) {
         if (message.equals("!!")) {
           return history.get(history.size() - 1);
@@ -57,36 +94,48 @@ public class Space2Slash extends PluginBase implements Listener {
           }
         }
       }
-      if (message.startsWith(" ")) {
-        message = message.trim();
-        history.add(message);
-        return message;
-      }
-      return null;
+      message = message.trim();
+      history.add(message);
+      return message;
     }
 
     @EventHandler (priority = EventPriority.LOWEST)
     public void onChatCommand (PlayerChatEvent event) {
-        String message = preprocessCommand(event.getMessage());
+        String message = event.getMessage();
+        if (message.startsWith("'")) {
+          event.setMessage(message.substring(1));
+          return;
+        }
+        message = preprocessCommand(event.getPlayer(), message);
         if (message != null) {
           this.getServer().dispatchCommand(event.getPlayer(), message);
           event.setCancelled();
         }
     }
 
+/*
     // This handler will be removed after fixing #197
     @EventHandler (priority = EventPriority.LOWEST)
     public void onCommandPreprocess (PlayerCommandPreprocessEvent event) {
-        String message = preprocessCommand(event.getMessage());
+        String message = event.getMessage();
+        if (message.startsWith("'")) {
+          event.setMessage(message.substring(1));
+          return;
+        }
+        message = preprocessCommand(event.getPlayer(), message);
         if (message != null) {
           this.getServer().dispatchCommand(event.getPlayer(), message);
           event.setCancelled();
         }
     }
+*/
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-      boolean accept = false;
+      if (!sender.isPlayer()) return false;
+      Player player = (Player)sender;
+      boolean accept = false;      
+      ArrayList<String> history = historyForPlayer(player);
       switch (command.getName()) {
       case "history":
         for (String arg : args) {
@@ -94,6 +143,9 @@ public class Space2Slash extends PluginBase implements Listener {
           case "--clear": case "-c":
             history.clear();
             accept = true;
+            break;
+          case "--save": case "-s":
+            saveHistory();
             break;
           }
         }
